72a9b7b4e88f18d40670cc90b8b244b5f1776158
Juan L <33398520+juanlucasl@users.noreply.github.com>
2022-10-01 13:47:22 -0300

    Set up mongodb connection

--- server/server.js
+++ server/server.js
@@ -4,20 +4,20 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
+const { MONGO_URI_STRING } = require('./~keys');
 
 const app = express();
 
-// Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = MONGO_URI_STRING;
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+  throw new Error('You must provide a MongoDB URI');
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+    .once('open', () => console.log('Connected to MongoDB instance.'))
+    .on('error', error => console.log('Error connecting to MongoDB:', error));
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
